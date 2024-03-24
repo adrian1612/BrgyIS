4c2c37abd23525c18bb8feb4d3471c498567b404
Lenovo <Lenovo@DESKTOP-GGN26CD>
2024-03-24 13:43:42 +0800

    Added db script

--- BrgyIS/databaseScript.sql
+++ BrgyIS/databaseScript.sql
@@ -1,23 +1,83 @@
-﻿CREATE DATABASE BrgyIS
+﻿USE [master]
 GO
-
-USE BrgyIS
+/****** Object:  Database [dbBrgyIS]    Script Date: 24/03/2024 1:43:06 pm ******/
+CREATE DATABASE [dbBrgyIS]
 GO
+USE [dbBrgyIS]
+GO
+/****** Object:  StoredProcedure [dbo].[tbl_Person_Proc]    Script Date: 24/03/2024 1:43:06 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+CREATE PROCEDURE [dbo].[tbl_Person_Proc]
+@Type VARCHAR(50),
+@Search VARCHAR(max) = null,
+@ID int = null,
+@fname varchar(max) = null,
+@mn varchar(max) = null,
+@lname varchar(max) = null,
+@bday datetime = null,
+@gender varchar(max) = null,
+@CivilStatus varchar(max) = null,
+@ShelterType varchar(500) = null,
+@Occupation varchar(max) = null,
+@isPWD bit = null,
+@RelationshipToHead varchar(50) = null,
+@StNo varchar(max) = null,
+@Address varchar(max) = null,
+@Remarks varchar(max) = null,
+@Encoder int = null,
+@Timestamp datetime = null
+AS
+BEGIN
+IF @Type = 'Create'
+BEGIN
+INSERT INTO [tbl_Person]
+([fname],[mn],[lname],[bday],[gender],[CivilStatus],[ShelterType],[Occupation],[isPWD],[RelationshipToHead],[StNo],[Address],[Remarks],[Encoder],[Timestamp])
+VALUES
+(@fname,@mn,@lname,@bday,@gender,@CivilStatus,@ShelterType,@Occupation,@isPWD,@RelationshipToHead,@StNo,@Address,@Remarks,@Encoder,@Timestamp)
 
-CREATE TABLE [dbo].[tbl_User](
-	[ID] [int] IDENTITY(1,1) NOT NULL,
-	[Username] [varchar](max) NULL,
-	[Password] [varchar](max) NULL,
-	[Role] [int] DEFAULT 2,
-	[Active] [bit] DEFAULT 1,
-	[fname] [varchar](max) NULL,
-	[mn] [varchar](max) NULL,
-	[lname] [varchar](max) NULL,
-	[gender] [varchar](50) NULL,
-	[email] [varchar](max) NULL,
-	[address] [varchar](max) NULL,
-	[Timestamp] [datetime] DEFAULT GETDATE()
-)
+END
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+IF @Type = 'Update'
+BEGIN
+UPDATE [tbl_Person] SET [fname] = @fname
+,[mn] = @mn
+,[lname] = @lname
+,[bday] = @bday
+,[gender] = @gender
+,[CivilStatus] = @CivilStatus
+,[ShelterType] = @ShelterType
+,[Occupation] = @Occupation
+,[isPWD] = @isPWD
+,[RelationshipToHead] = @RelationshipToHead
+,[StNo] = @StNo
+,[Address] = @Address
+,[Remarks] = @Remarks
+,[Encoder] = @Encoder
+,[Timestamp] = @Timestamp WHERE [ID] = @ID
+END
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+IF @Type = 'Search'
+BEGIN
+SELECT * FROM [tbl_Person] 
+END
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+IF @Type = 'Find'
+BEGIN
+SELECT * FROM [tbl_Person] WHERE  ID = @ID
+END
+--------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+END
+
+
+GO
+/****** Object:  StoredProcedure [dbo].[tbl_User_Proc]    Script Date: 24/03/2024 1:43:06 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE PROCEDURE [dbo].[tbl_User_Proc]
@@ -102,8 +162,88 @@ BEGIN
 	SELECT * FROM [tbl_User] WHERE HASHBYTES('MD5', Username) = HASHBYTES('MD5', @Username) AND HASHBYTES('MD5', [Password]) = HASHBYTES('MD5', @Password) 
 END
 END
-
+GO
+/****** Object:  Table [dbo].[tbl_Person]    Script Date: 24/03/2024 1:43:06 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[tbl_Person](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[fname] [varchar](max) NULL,
+	[mn] [varchar](max) NULL,
+	[lname] [varchar](max) NULL,
+	[bday] [datetime] NULL,
+	[gender] [varchar](max) NULL,
+	[CivilStatus] [varchar](max) NULL,
+	[ShelterType] [varchar](500) NULL,
+	[Occupation] [varchar](max) NULL,
+	[isPWD] [bit] NULL,
+	[RelationshipToHead] [varchar](50) NULL,
+	[StNo] [varchar](max) NULL,
+	[Address] [varchar](max) NULL,
+	[Remarks] [varchar](max) NULL,
+	[Encoder] [int] NULL,
+	[Timestamp] [datetime] NULL,
+PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
 GO
+SET ANSI_PADDING OFF
+GO
+/****** Object:  Table [dbo].[tbl_User]    Script Date: 24/03/2024 1:43:06 pm ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+SET ANSI_PADDING ON
+GO
+CREATE TABLE [dbo].[tbl_User](
+	[ID] [int] IDENTITY(1,1) NOT NULL,
+	[Username] [varchar](max) NULL,
+	[Password] [varchar](max) NULL,
+	[Role] [int] NULL,
+	[Active] [bit] NULL,
+	[fname] [varchar](max) NULL,
+	[mn] [varchar](max) NULL,
+	[lname] [varchar](max) NULL,
+	[gender] [varchar](50) NULL,
+	[email] [varchar](max) NULL,
+	[address] [varchar](max) NULL,
+	[Timestamp] [datetime] NULL
+) ON [PRIMARY] TEXTIMAGE_ON [PRIMARY]
 
+GO
+SET ANSI_PADDING OFF
+GO
+SET IDENTITY_INSERT [dbo].[tbl_Person] ON 
 
+GO
+INSERT [dbo].[tbl_Person] ([ID], [fname], [mn], [lname], [bday], [gender], [CivilStatus], [ShelterType], [Occupation], [isPWD], [RelationshipToHead], [StNo], [Address], [Remarks], [Encoder], [Timestamp]) VALUES (1, N'Adrian', N'Aranilla', N'Jaspio', CAST(0x000086D600000000 AS DateTime), N'Male', N'Married', N'Rented', N'Computer Programmer', 0, N'Head of the Family', N'Kahusayan st', N'Pamana Homes sub kahusayan st, brgy bucal', NULL, 1, NULL)
+GO
+SET IDENTITY_INSERT [dbo].[tbl_Person] OFF
+GO
+SET IDENTITY_INSERT [dbo].[tbl_User] ON 
+
+GO
+INSERT [dbo].[tbl_User] ([ID], [Username], [Password], [Role], [Active], [fname], [mn], [lname], [gender], [email], [address], [Timestamp]) VALUES (1, N'admin', N'admin!!@@', 2, 1, N'adrian', N'aranilla', N'jaspio', N'Male', N'adrianjaspio@gmail.com', N'purok santol 1 mayao crossing lucena city', CAST(0x0000B13D00DA7267 AS DateTime))
+GO
+SET IDENTITY_INSERT [dbo].[tbl_User] OFF
+GO
+ALTER TABLE [dbo].[tbl_Person] ADD  DEFAULT (getdate()) FOR [Timestamp]
+GO
+ALTER TABLE [dbo].[tbl_User] ADD  DEFAULT ((2)) FOR [Role]
+GO
+ALTER TABLE [dbo].[tbl_User] ADD  DEFAULT ((1)) FOR [Active]
+GO
+ALTER TABLE [dbo].[tbl_User] ADD  DEFAULT (getdate()) FOR [Timestamp]
+GO
+USE [master]
+GO
+ALTER DATABASE [dbBrgyIS] SET  READ_WRITE 
+GO
