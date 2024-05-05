1d77d8b489fb60df551b9b5944c76e27101ffe41
Lenovo <Lenovo@DESKTOP-GGN26CD>
2024-05-05 12:00:14 +0800

    Update

--- BrgyIS/Controllers/HomeController.cs
+++ BrgyIS/Controllers/HomeController.cs
@@ -11,9 +11,29 @@ namespace BrgyIS.Controllers
     public class HomeController : Controller
     {
         tbl_User mod = new tbl_User();
+        tbl_Person person = new tbl_Person();
         public ActionResult Index()
         {
             return View();
+        }
+
+
+        public JsonResult GenderStatistic()
+        {
+            var list = person.List();
+            var gender = new { Male = list.Count(f => f.gender == "Male"), Female = list.Count(f => f.gender == "Female") };
+            return new JsonNetResult { Data = gender };
+        }
+
+        public JsonResult SrPwdStatistic()
+        {
+            var list = person.List();
+            var gender = new
+            {
+                Senior = list.Count(f => f.isSenior),
+                PWD = list.Count(f => f.isPWD)
+            };
+            return new JsonNetResult { Data = gender };
         }
 
         public ActionResult RestrictedAccess()
